e09e3781877cfed29530baded6a07e4df033f295
mpinzon <mspinzon406@misena.edu.co>
2022-07-22 11:54:25 -0500

    Proyecto iniciado

---
+++ app/Models/Continent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Continent extends Model
+{
+    //La tabla con la cual el modelo se relaciona:
+    protected $table = "continents";
+    //La clave primaria de la tabla
+    protected $primaryKey = "continent_id";
+    //Anular los campos de auditoria
+    public $timestamps = false;
+    use HasFactory;
+}
